2bae811eef799c6c8ff6cd5d2f4f1a2c6684461e
Colin Henry <jchenry@users.noreply.github.com>
2020-11-06 17:39:39 -0800

    added GetAllWorkouts

--- workout.go
+++ workout.go
@@ -22,3 +22,30 @@ func GetWorkouts(c Client, limit, page int) (model.WorkoutHistory, error) {
 	err = json.Unmarshal(body, &wh)
 	return wh, err
 }
+
+func GetAllWorkouts(c Client) (model.WorkoutHistory, error) {
+	resp, _ := c.HTTPClient.Get(fmt.Sprintf("%s/api/user/%s/workouts?joins=ride&limit=%d&page=%d", apiBase, c.user.ID, 10, 0))
+
+	twh := model.WorkoutHistory{}
+
+	index := struct {
+		PageCount int `json:"page_count"`
+		PageSize  int `json:"limit"`
+	}{}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return twh, err
+	}
+
+	err = json.Unmarshal(body, &index)
+
+	for p := 0; p < index.PageCount; p++ {
+		wh, err := GetWorkouts(c, index.PageSize, p)
+		if err != nil {
+			return twh, err
+		}
+		twh.Workouts = append(twh.Workouts, wh.Workouts...)
+	}
+
+}
